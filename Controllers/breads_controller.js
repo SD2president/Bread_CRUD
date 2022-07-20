e3e4c20e174909293d3e5b61f0aac99ee1bc5d87
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2022-07-20 00:03:46 -0700

    I put edit and updates routes complete

--- Controllers/breads_controller.js
+++ Controllers/breads_controller.js
@@ -18,7 +18,7 @@ breads.post('/', (req, res) => {
   if (!req.body.image) {
     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   }
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
@@ -33,8 +33,28 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 // DELETE
-breads.delete('/:indexArray', (req,res) => {
+breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
@@ -43,7 +63,7 @@ breads.delete('/:indexArray', (req,res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
+      bread: Bread[req.params.arrayIndex],
       index: req.params.arrayIndex,
     })
   } else {
